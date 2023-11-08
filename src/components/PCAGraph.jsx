4360c303c689da918c40b363aa5d73ca83a49996
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-11-08 19:51:18 +0000

    create functioning color dropdown

--- src/components/PCAGraph.jsx
+++ src/components/PCAGraph.jsx
@@ -3,38 +3,49 @@ import HighchartsReact from "highcharts-react-official";
 import HC_exporting from "highcharts/modules/exporting";
 import highchartsAccessibility from "highcharts/modules/accessibility";
 import HC_annotations from "highcharts/modules/annotations";
-// initializing Highcharts with additional modules
+import { useState, useEffect } from "react";
+
+// Initializing Highcharts with additional modules
 highchartsAccessibility(Highcharts);
 HC_exporting(Highcharts);
 HC_annotations(Highcharts);
 
-function PCAGraph({ pcaData, scoresData, parsedSampleInfo, selectedPCs }) {
+function PCAGraph({
+  pcaData,
+  scoresData,
+  parsedSampleInfo,
+  selectedPCs,
+  colorBy,
+}) {
+  // State to manage the chart's options
+  const [chartOptions, setChartOptions] = useState(null);
+
+  let colorIdx = 0;
+  const seriesData = {};
+  const initialAnnotations = [];
   const colorsArray = [
     "rgb(254,125,43)",
     "rgb(60,89,193)",
-    "rgb(0,128,0)" /* Add more colors if needed */,
+    "rgb(0,128,0)", // Add more colors if needed
   ];
-  let colorIdx = 0;
-
-  const seriesData = {};
-  let initialAnnotations = []; // Array to hold initial annotations
 
   // Sort the selected PCs in ascending order
   const sortedPCs = [...selectedPCs].sort((a, b) => a - b);
 
-  parsedSampleInfo.forEach((sample) => {
-    if (!seriesData[sample.condition]) {
-      seriesData[sample.condition] = {
-        name: sample.condition,
+  useEffect(() => {
+    setChartOptions({ ...options });
+  }, [colorBy]); // Rerun when colorBy changes
+
+  parsedSampleInfo.forEach((sample, idx) => {
+    if (!seriesData[sample[colorBy]]) {
+      seriesData[sample[colorBy]] = {
+        name: sample[colorBy],
         color: colorsArray[colorIdx % colorsArray.length], // Assign a color from the array
         data: [],
       };
 
       colorIdx++;
     }
-  });
-
-  parsedSampleInfo.forEach((sample, idx) => {
     const point = {
       x: scoresData[idx][sortedPCs[0] - 1],
       y: scoresData[idx][sortedPCs[1] - 1],
@@ -42,7 +53,7 @@ function PCAGraph({ pcaData, scoresData, parsedSampleInfo, selectedPCs }) {
       id: "point" + idx, // Unique ID for the point
       ...sample,
     };
-    seriesData[sample.condition].data.push(point);
+    seriesData[sample[colorBy]].data.push(point);
 
     // Add a default annotation for each point
     initialAnnotations.push({
@@ -138,12 +149,12 @@ function PCAGraph({ pcaData, scoresData, parsedSampleInfo, selectedPCs }) {
                 // Remove the existing annotation using the annotation's ID
                 chart.removeAnnotation(annotation.options.id);
               } else {
-                // Add a new annotation with a unique ID
+                // Add a new annotation for the point
                 chart.addAnnotation({
-                  id: "annotation-" + this.id, // Unique ID for the annotation
+                  id: "annotation-" + this.id,
                   labels: [
                     {
-                      point: this.id, // Use the point ID to refer to the point
+                      point: this.id,
                       text: this.name,
                       backgroundColor: "rgba(255,255,255,0.5)",
                       borderColor: "black",
@@ -172,7 +183,7 @@ function PCAGraph({ pcaData, scoresData, parsedSampleInfo, selectedPCs }) {
       },
     },
     series: Object.values(seriesData),
-    annotations: initialAnnotations, // Include the default annotations
+    annotations: initialAnnotations,
   };
 
   return (
