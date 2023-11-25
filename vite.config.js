f3218670c04176624bee7c46144c5ce94dfbfba9
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-11-25 22:26:51 +0000

    configure vite to allow packaging of app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    lib: {
+      entry: "src/exports.js", // Your library's entry point
+      name: "pca-plot", // The name of your library
+      formats: ["es", "umd"], // Output formats
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"], // Do not bundle React and ReactDOM
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
